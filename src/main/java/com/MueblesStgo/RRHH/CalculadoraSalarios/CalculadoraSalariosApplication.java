d3aa6d85317f43ac7b7d3760b0ded5154e76645b
Miguel Villalobos <miguel.villalobos@usach.cl>
2022-09-03 17:11:24 -0400

    Generación del proyecto
    Usando la página spring initializr, con las dependencias: Spring Web, MySQL Driver, Spring Data JPA, Thymeleaf y Lombok

---
+++ src/main/java/com/MueblesStgo/RRHH/CalculadoraSalarios/CalculadoraSalariosApplication.java
@@ -0,0 +1,13 @@
+package com.MueblesStgo.RRHH.CalculadoraSalarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraSalariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraSalariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MueblesStgo/RRHH/CalculadoraSalarios/CalculadoraSalariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MueblesStgo.RRHH.CalculadoraSalarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraSalariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
