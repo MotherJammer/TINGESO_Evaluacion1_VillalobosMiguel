b104563138094b074ee70c7db3b2d0d9e72bc3e8
Miguel Villalobos <miguel.villalobos@usach.cl>
2022-09-08 14:24:09 -0400

    Datos falsos creados
    Se crearon los marcajes falsos de los empleados agregados a la base de datos. También se hizo un update a informaciónes de un par de empleados para poder abarcar las reglas de negocio a probar en el programa

--- SQL/loadData.sql
+++ SQL/loadData.sql
@@ -6,8 +6,8 @@ VALUES
 
 INSERT INTO `empleado` (`rut`,`apellidos`,`nombre`,`fecha_nac`,`fecha_ing`,`categoria`)
 VALUES
-  ("24321441-6","Sanchez Renato","Norman MacKenzie","1970-02-26 06:42:46","2017-02-24 20:49:22", 1),
-  ("9827441-3","Nicolas Alexandra","Deirdre Xyla","1992-04-02 18:44:33","2017-03-16 03:10:27", 2),
-  ("31547806-5","Laura Augustin","Talon Simone","1977-08-24 22:26:54","2016-04-29 16:52:24", 3),
-  ("3641673-4","Gabriela Vargas","Hyatt Austin","1983-12-25 07:27:57","2019-09-04 11:44:13", 2),
-  ("24688721-7","Zavala Contreras","Mary Ferdinand","1986-06-04 10:36:35","2019-07-29 17:01:06", 3);
+  ("24.321.441-6","Sanchez Renato","Norman MacKenzie","1970-02-26 06:42:46","2017-02-24 20:49:22", 1),
+  ("9.827.441-3","Nicolas Alexandra","Deirdre Xyla","1992-04-02 18:44:33","2017-03-16 03:10:27", 2),
+  ("31.547.806-5","Laura Augustin","Talon Simone","1977-08-24 22:26:54","2016-04-29 16:52:24", 3),
+  ("3.641.673-4","Gabriela Vargas","Hyatt Austin","1983-12-25 07:27:57","2010-09-04 11:44:13", 2),
+  ("24.688.721-7","Zavala Contreras","Mary Ferdinand","1986-06-04 10:36:35","2019-07-29 17:01:06", 3);
